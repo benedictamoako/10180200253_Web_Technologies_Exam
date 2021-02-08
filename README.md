abcc592b31e02c1960130176f92313f5b77c37e9
bennyamk <amoakobenedict@gmail.com>
2021-02-08 01:59:06 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# 10180200253_Web_Technologies_Exam
---
+++ assests/js/main.js
@@ -0,0 +1,52 @@
+var boseModal = new bootstrap.Modal(document.getElementById("boseModal"));
+var focalUtopiaModal = new bootstrap.Modal(document.getElementById("focalUtopiaModal"));
+var sennheiserModal = new bootstrap.Modal(document.getElementById("sennheiserModal"));
+var logitechModal = new bootstrap.Modal(document.getElementById("logitechModal"));
+var focalStelliaModal = new bootstrap.Modal(document.getElementById("focalStelliaModal"));
+var focalStelliaModal = new bootstrap.Modal(document.getElementById("focalStelliaModal"));
+var jabraModal = new bootstrap.Modal(document.getElementById("jabraModal"));
+var boseBuy = document.getElementById("bose-buy");
+var boseBuy2 = document.getElementById("bose-buy-2");
+var focalUtopiaButton = document.getElementById("focalUtopiaButton");
+var focalUtopiaButton2 = document.getElementById("focalUtopiaButton2");
+var sennheiserButton = document.getElementById("sennheiserButton");
+var sennheiserautton2 = document.getElementById("sennheiserButton2");
+var logitechButton = document.getElementById("logitechButton");
+var focalStelliaButton = document.getElementById("focalStelliaButton");
+var jabraButton = document.getElementById("jabraButton");
+
+boseBuy.addEventListener('click',function(){
+    boseModal.show();
+})
+
+boseBuy2.addEventListener('click',function(){
+    boseModal.show();
+})
+
+focalUtopiaButton.addEventListener('click',function(){
+    focalUtopiaModal.show();
+})
+
+focalUtopiaButton2.addEventListener('click',function(){
+    focalUtopiaModal.show();
+})
+
+sennheiserButton.addEventListener('click',function(){
+    sennheiserModal.show();
+})
+
+sennheiserButton2.addEventListener('click',function(){
+    sennheiserModal.show();
+})
+
+logitechButton.addEventListener('click',function(){
+    logitechModal.show();
+})
+
+focalStelliaButton.addEventListener('click',function(){
+    focalStelliaModal.show();
+})
+
+jabraButton.addEventListener('click',function(){
+    jabraModal.show();
+})
